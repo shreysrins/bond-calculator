061e3cc6b1bcfb870d0a245e2830d5fe1e6c0616
Shreyas Srinivasan <shreyass@mit.edu>
2021-04-05 21:13:28 -0400

    YTM calculation working

---
+++ bond_calc.py
@@ -0,0 +1,69 @@
+"""Bond Calculator
+
+This script allows the user to calculate the Yield-to-Maturity, Price, Duration, or Convexity of a bond. It is operated purely through a Command Line Interface (CLI).
+"""
+
+from pyfiglet import Figlet
+import numpy as np
+from scipy.optimize import newton
+
+
+def _cash_flows(discount: float = 0.0, coupon: float = 0.0, face: float = 100.00, nper: int = 1) -> float:
+    """Calculates the present value of a bond's cash flows.
+
+    Parameters
+    ----------
+    discount : float = 0.0
+        The discount rate for the cash flows (equivalently, the bond's yield)
+    coupon : float = 0.0
+        The coupon rate of the bond.
+    face : float = 100.00
+        The bond's face value, or principal amount repaid at maturity.
+    nper : int = 1
+        The number of payment periods.
+
+    Returns
+    -------
+    float
+        The present value (PV) of the bond's cash flows.
+    """
+    cf = np.fromfunction(lambda i: coupon*face/(1 + discount)**(i + 1), (nper,), dtype=int)
+    cf[-1] += face/(1 + discount)**(cf.shape[0]) # Acount for principal repayment in last period
+    return np.sum(cf) # Return PV(Cash Flows)
+
+
+def _ytm() -> float:
+    """Calculates a bond's Yield-to-Maturity (YTM).
+
+    Acquires all necessary parameters through command line input.
+
+    Returns
+    -------
+    float
+        The YTM of the desired bond, as a decimal.
+    """
+    price = float(input("What is the price of this bond? $"))
+    nper = int(input("What is the total number of payment periods? "))
+    face = float(input("What is the face value of the bond? $"))
+    coupon = float(input("What is the coupon %age per period? Enter as a number (e.g. '5' for 5%) "))/100
+
+    return newton(lambda y: _cash_flows(y, coupon, face, nper) - price, 0.05)
+
+
+if __name__ == '__main__':
+    f = Figlet(font='slant')
+    print(f.renderText('Bond Calculator'))
+
+    exit = False
+    while(not exit):
+        print("---------------------------------------------")
+        print("Please select one of the following functions:")
+        print("    1. YTM")
+        print("    2. Price")
+        print("    3. Duration & Convexity")
+        choice = int(input("Enter your choice as a number: "))
+
+        if choice == 1:
+            print("Yield to Maturity: {:.2f}%".format(100*_ytm()))
+
+        exit = (input("Press 'x' to exit or any key to continue: ").strip() == "x")
